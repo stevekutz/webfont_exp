03b5bf9592f60f33044754204cd163d3b4ce6ba2
stevekutz <stkutz@gmail.com>
2020-01-25 22:33:29 -0600

    imported ttf into App

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import './styles/stylesheet.css';
+
+import  {
+    MyDiv,
+
+} from './styledComp/myStyledComp';
+
 
 function App() {
   return (
@@ -11,6 +17,19 @@ function App() {
         <p style = {{fontFamily: 'climbing_plantregular', textAlign: 'center'}}>
             p is special font styled
         </p>
+        <p style = {{
+            fontFamily: 'a-song-for-jennifer'
+        }}> A song for Jennifer font</p>
+        <p style = {{
+            fontFamily: 'Kahlesv2'
+        }}> Kahless font</p>
+        <p style = {{
+            fontFamily: 'Open-24-Display-St'
+        }}> Open-24-Display-St font</p>
+
+
+        <MyDiv> Here is MyDiv</MyDiv>
+
     </div>
   );
 }
---
+++ app/src/styledComp/myStyledComp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+
+const MyDiv = styled.div`
+    border: 1px solid deeppink;
+    display: flex;
+    justify-content: center;
+    font-family: climbing_plantregular;
+
+
+
+`;
+
+export {
+    MyDiv,
+
+}
