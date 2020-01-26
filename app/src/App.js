41a5196f2c4beaf8d46b57b88e9e1d803c43f178
stevekutz <stkutz@gmail.com>
2020-01-26 13:03:48 -0600

    demonstrated font import  with inline styling, stylesheets, $ styled components

--- app/src/App.js
+++ app/src/App.js
@@ -12,21 +12,25 @@ import  {
 function App() {
   return (
     <div className="App">
-        <h1> styled h1</h1>
-        <h2> styled h2</h2>
+        <h1> h1 styled selector h1, h2 in App.css</h1>
+        <h2> h1 styled selector h1, h2 in App.css</h2>
         <p style = {{fontFamily: 'climbing_plantregular', textAlign: 'center'}}>
-            p is special font styled
+            p is climbing_plantregular with inline styling
         </p>
         <p style = {{
             fontFamily: 'a-song-for-jennifer'
-        }}> A song for Jennifer font</p>
+        }}> A song for Jennifer font, font filename had spaces, replace with dashes, also inline styling</p>
 
         <p style = {{
             fontFamily: 'Open-24-Display-St'
         }}> Open-24-Display-St font</p>
 
 
-        <MyDiv> Here is Kahless font in MyDiv</MyDiv>
+        <MyDiv> Kahless font used in styled component MyDiv</MyDiv>
+
+        <p style = {{
+            fontFamily: 'Hanged-Letters'
+        }}> Hanging Letters imported into /index.css</p>
 
     </div>
   );
